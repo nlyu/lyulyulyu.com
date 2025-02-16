903e717f3515cd0876d5c748990fdfeda05b6443
Nuochen Lyu <nulyu@microsoft.com>
2025-02-15 18:38:31 -0800

    working first

---
+++ lyulyulyu/Components/Resources/ChaiManager.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lyulyulyu.Components.Resources
+{
+    public interface IChaiManager
+    {
+        List<Resource> GetImages();
+        int GetImageCount();
+    }
+    public class ChaiManager : IChaiManager
+    {
+        private readonly IWebHostEnvironment _env;
+        private List<Resource> resouce;
+
+        public ChaiManager(IWebHostEnvironment webHostEnvironment)
+        {
+            _env = webHostEnvironment;
+            this.GetImages();
+        }
+        public List<Resource> GetImages()
+        {
+            if (resouce != null)
+            {
+                return resouce;
+            }
+
+            string[] filePaths = Directory.GetFiles(Path.Combine(_env.WebRootPath, @"Chai"));
+            List<Resource> pptlist = new List<Resource>();
+            foreach (string filePath in filePaths)
+            {
+                var ppt = new Resource();
+                ppt.DisplayPath = @"Chai/" + Path.GetFileNameWithoutExtension(filePath) + @".png";
+                ppt.DownloadPath = @"Chai/download/" + Path.GetFileNameWithoutExtension(filePath) + @".pptx";
+                ppt.Name = Path.GetFileNameWithoutExtension(filePath);
+                ppt.Type = Path.GetExtension(filePath);
+                pptlist.Add(ppt);
+            }
+
+            this.resouce = pptlist;
+            return pptlist;
+        }
+
+        public int GetImageCount()
+        {
+            return this.resouce.Count;
+        }
+    }
+}
---
+++ lyulyulyu/Program.cs
@@ -0,0 +1,32 @@
+using lyulyulyu.Components;
+using lyulyulyu.Components.Resources;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<IChaiManager, ChaiManager>();
+builder.Services.AddScoped<ICityDoodleManager, CityDoodleManager>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
